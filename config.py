da1b2c2af292c1988470c913cf3c35e1dc864abc
rj3316 <i.usunariz.lopez@gmail.com>
2021-12-17 11:37:07 +0100

    Set temporal wallpaper

--- config.py
+++ config.py
@@ -81,6 +81,10 @@ keys = [
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
     Key([mod], "r", lazy.spawncmd(),
         desc="Spawn a command using a prompt widget"),
+    
+    # Menu
+    Key([mod], "m", lazy.spawn("rofi -show run")),
+    Key([mod, "shift"], "m", lazy.spawn("rofi -show")),
 ]
 
 groups = [Group(i) for i in "123456789"]
